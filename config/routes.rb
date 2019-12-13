2a9c9a043bd69cdd53fc210f0c65fa09f720e63b
ombogdan <omelcenko44@gmail.com>
2019-12-13 17:27:26 +0200

    Add CRUD for lectures

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/chop-chop-edu', to: 'courses#homepage'
 
-  resources :courses, path: '/chop-chop-edu/coursess'
+  resources :courses, path: '/chop-chop-edu/courses'
   get '/chop-chop-edu/courses-for-group', to: 'courses#forgroup'
 
 
@@ -19,6 +19,10 @@ Rails.application.routes.draw do
   resources :teachers, path: '/chop-chop-edu/teachers'
   get '/chop-chop-edu/teachers', to: 'teachers#all'
   get '/chop-chop-edu/teachers-for-courses', to: 'teachers#forcourses'
+
+
+  resources :labs, path: '/chop-chop-edu/labs'
+
 
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_090845) do
+ActiveRecord::Schema.define(version: 2019_12_13_122052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "courses", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "labs", force: :cascade do |t|
+    t.string "theme_name", null: false
+    t.integer "subject_theme_id"
+    t.string "goal", null: false
+    t.text "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
