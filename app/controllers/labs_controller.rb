3d00013f6488afcdaf5109782273db1a9b124a25
ombogdan <36894763+ombogdan@users.noreply.github.com>
2019-12-22 17:35:52 +0200

    Add select for lecture practice and labs

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -1,52 +1,53 @@
 class LabsController < ApplicationController
-    def new
-      @labs = Lab.new
+  def new
+    @labs = Lab.new
+    @themes = SubjectTheme.all
+  end
+
+  def create
+    @lab = Lab.new(lab_params)
+    if @lab.save
+      redirect_to labs_path
+    else
+      render :new
     end
+  end
 
-    def create
-      @lab = Lab.new(lab_params)
-      if @lab.save
-        redirect_to labs_path
-      else
-        render :new
-      end
+  def index
+    @labs = Lab.all
+  end
+
+  def edit
+    @lab = Lab.find(params[:id])
+  end
+
+  def show
+    @labs = Lab.find(params[:id])
+  end
+
+  def update
+    @labs = Lab.find(params[:id])
+
+    if @labs.update(lab_params)
+      redirect_to @labs
+    else
+      render 'edit'
     end
+  end
 
-    def index
-      @labs = Lab.all
+  def destroy
+    @lab = Lab.find(params[:id])
+    if @lab.destroy
+      redirect_to labs_path
+    else
+      redirect_to labs_path, error: "This lab not delete"
     end
+  end
 
-    def edit
-      @lab = Lab.find(params[:id])
-    end
+  private
 
-    def show
-      @labs = Lab.find(params[:id])
-    end
-
-    def update
-      @labs = Lab.find(params[:id])
-
-      if @labs.update(lab_params)
-        redirect_to @labs
-      else
-        render 'edit'
-      end
-    end
-
-    def destroy
-      @lab = Lab.find(params[:id])
-      if @lab.destroy
-        redirect_to labs_path
-      else
-        redirect_to labs_path, error: "This lab not delete"
-      end
-    end
-
-    private
-
-    def lab_params
-      params[:lab].permit(:theme_name, :subject_theme_id, :goal, :text)
-    end
+  def lab_params
+    params[:lab].permit(:theme_name, :subject_theme_id, :goal, :text)
+  end
 
 end
