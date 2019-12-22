7f93ce6c6194cdbe50f702a69eb8abb59f8523ba
ombogdan <36894763+ombogdan@users.noreply.github.com>
2019-12-22 13:53:23 +0200

    Add devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :students
+  devise_for :users
   get 'home/index'
 
   root 'home#index'
--- db/migrate/20191214124636_add_groupname_to_students.rb
+++
@@ -1,6 +0,0 @@
-class AddGroupnameToStudents < ActiveRecord::Migration[6.0]
-  def change
-    add_column :students, :groupname, :string
-    add_index :students, :groupname, unique: true
-  end
-end
